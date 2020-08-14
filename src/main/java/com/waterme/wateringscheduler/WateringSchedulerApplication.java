eeaf91313125bb250e5a0f325f415a73080ebfcc
miguel plancarte <plancarte2@gmail.com>
2020-08-14 14:00:29 -0500

    Initial commit

---
+++ src/main/java/com/waterme/wateringscheduler/WateringSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.waterme.wateringscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WateringSchedulerApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(WateringSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/waterme/wateringscheduler/WateringSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waterme.wateringscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WateringSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
